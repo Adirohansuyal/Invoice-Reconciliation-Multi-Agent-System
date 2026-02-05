69a0dbe1068a3bb2aa10a3d51a42a470989db626
ohanSuyal <144371692+Adirohansuyal@users.noreply.github.com>
2026-02-05 18:43:00 +0530

    Last Update test successfull

--- app.py
+++ app.py
@@ -195,17 +195,8 @@ with events_col:
     st.subheader("Version History")
     st.markdown(
         """
-        <style>
-            @keyframes attention-border {
-                0%, 100% {
-                    border-color: #800000;
-                }
-                50% {
-                    border-color: #ff4d4d; /* A brighter, more attention-grabbing red */
-                }
-            }
-        </style>
-        <div style="background-color: #1C4E38 ; color: white; padding: 1em; border-radius: 10px; border: 3px solid #800000; animation: attention-border 2s ease-in-out infinite;">
+        
+        <div style="background-color: #800000 ; color: white; padding: 1em; border-radius: 10px; border: 3px solid #800000; animation: attention-border 2s ease-in-out infinite;">
         
         **v1.2 - Feb 04, 2026**
         - Added CI gated-secured CD Deployment Feature.
